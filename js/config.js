1a5d8cfafdad063880e2cb28e5c432893530cf66
NiTrOxz0r <nrxzor@gmail.com>
2017-12-26 14:02:28 -0400

    BringerTech El comienzo

---
+++ js/config.js
@@ -0,0 +1,39 @@
+/*===============================================
+         Servicios  Animación con el SCROLL
+ ================================================*/
+                $(document).ready(function(){
+                
+                    //Animate Scroll
+                    new WOW().init();
+                
+                });     
+
+/*===============================================
+                        Work
+================================================*/
+$(document).ready(function(){
+
+    $("#trabajo").magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+});
+
+/*===============================================
+                        Team
+================================================*/
+$(document).ready(function(){
+
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+
+});
