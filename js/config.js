2b0fd2763c38bc012dcffa524383057d126e7f0c
NiTrOxz0r <nrxzor@gmail.com>
2017-12-26 15:46:47 -0400

    Modifcaciones en el index (Trabajo,Equipo,Clientes,Footer)

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@
                 });     
 
 /*===============================================
-                        Work
+                    Trabajo
 ================================================*/
 $(document).ready(function(){
 
@@ -24,7 +24,7 @@ $(document).ready(function(){
 });
 
 /*===============================================
-                        Team
+                     Equipo
 ================================================*/
 $(document).ready(function(){
 
@@ -34,6 +34,66 @@ $(document).ready(function(){
         smartSpeed: 700,
         loop: true,
         autoplayHoverPause: true
+    });
+
+});
+
+ /*===============================================
+                       Stats
+ ================================================*/
+    $(document).ready(function(){
+
+        $(".counter").counterUp({
+            delay: 10,
+            time: 3000
+        });
+
+    });
+
+/*===============================================
+                       Clientes
+ ================================================*/
+        $(document).ready(function(){
+
+            $("#clients-list").owlCarousel({
+                items: 6,
+                autoplay: true,
+                smartSpeed: 700,
+                loop: true,
+                autoplayHoverPause: true
+            });
+
+        });
+
+ /*===============================================
+                        Smooth-Scroll
+================================================*/
+
+$("a.smooth-scroll").click( function(evento){
+
+    evento.preventDefault();
+    var section = $(this).attr("href");
+
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+
+}, 0);
+
+/*===============================================
+                        Button Top
+================================================*/
+
+$(function(){
+
+    $(window).scroll(function() {
+
+        if($(this).scrollTop() < 50) {
+            $("#back-to-top").fadeOut();
+        }else{
+            $("#back-to-top").fadeIn();
+        }
+
     });
 
 });
