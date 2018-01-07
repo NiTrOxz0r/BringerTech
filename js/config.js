27d69558eaf7c8e4a0a6b486a0e5f823deb10f3d
NiTrOxz0r <nrxzor@gmail.com>
2018-01-07 02:09:12 -0400

    Nivo Slider

--- js/config.js
+++ js/config.js
@@ -97,3 +97,38 @@ $(function(){
     });
 
 });
+
+/*===============================================
+                        Slider Principal
+================================================*/
+
+$(window).on('load', function() {
+    $('#slider').nivoSlider(); 
+}); 
+
+
+// /*<!-- load jQuery 1.7.1 -->*/
+// var jQuery_1_7_1 = $.noConflict(true);
+// /*<!-- load jQuery 3.2.1 -->*/
+// var jQuery_3_2_1 = $.noConflict(false);
+
+// var $jQuery_1_7_1 = jQuery.noConflict();
+//          $jQuery_1_7_1(document).ready(function() {
+//           $jQuery_1_7_1("#slider").nivoSlider({
+                
+//                 keyPress:true,
+//         item:4,
+//             });
+            
+//     });
+
+//          var $jQuery_3_2_1 = jQuery.noConflict();
+//          $jQuery_3_2_1(document).ready(function() {
+//          $jQuery_3_2_1("#clients-list").owlCarousel({
+                
+//                 keyPress:true,
+//         item:4,
+//             });
+            
+//     });
+
